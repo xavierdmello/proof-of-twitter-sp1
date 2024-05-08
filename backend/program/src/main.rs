8625b830ae1985e5eb1d2eebfc89c0bce6a46545
Xavier DMello <xaviermdmello@gmail.com>
2024-05-07 21:26:51 -0400

    clean up program

--- backend/program/src/main.rs
+++ backend/program/src/main.rs
@@ -1,10 +1,10 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
-use serde::{Deserialize, Serialize};
-use sha2::{Sha256, Digest};
 use base64::prelude::*;
-use rsa::{pkcs8::DecodePublicKey, RsaPublicKey, Pkcs1v15Sign};
 use regex::Regex;
+use rsa::{pkcs8::DecodePublicKey, Pkcs1v15Sign, RsaPublicKey};
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct DKIM {
@@ -29,7 +29,11 @@ pub fn main() {
     let from_address_verified = verify_from_address(&dkim);
     let is_pw_reset_email = verify_pw_reset_email(&dkim);
     let twitter_username = get_twitter_username(&dkim); // blank string if no/invalid twitter username
-    let twitter_proved = body_verified && signature_verified && from_address_verified && is_pw_reset_email && twitter_username.len() > 0;
+    let twitter_proved = body_verified
+        && signature_verified
+        && from_address_verified
+        && is_pw_reset_email
+        && twitter_username.len() > 0;
 
     sp1_zkvm::io::commit(&twitter_username);
     sp1_zkvm::io::commit(&crypto_address);
@@ -37,7 +41,6 @@ pub fn main() {
 }
 
 fn verify_body(dkim: &DKIM) -> bool {
-
     // get sha256 hash of body
     let mut hasher = Sha256::new();
     hasher.update(dkim.body.as_bytes());
@@ -58,20 +61,27 @@ fn verify_signature(dkim: &DKIM) -> bool {
     let hash = hasher.finalize();
 
     // 2. decode the public key from PEM format
-    let public_key = RsaPublicKey::from_public_key_pem(&dkim.public_key).unwrap();
+    let public_key = RsaPublicKey::from_public_key_pem(&dkim.public_key)
+        .expect("error decoding public key into PEM format");
 
     // 3. decode the signature from base64 into binary
-    let signature = BASE64_STANDARD.decode(&dkim.signature).unwrap();
+    let signature = BASE64_STANDARD
+        .decode(&dkim.signature)
+        .expect("error decoding signature into binary");
 
     // 4. verify the signature
     // RSASSA-PKCS1-V1_5 magic padding bytes
     // https://crypto.stackexchange.com/questions/86385/initial-value-for-rsa-and-sha-256-signature-encoding
     let prefix: Box<[u8]> = Box::new([
-        0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20
+        0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01, 0x65, 0x03, 0x04, 0x02, 0x01,
+        0x05, 0x00, 0x04, 0x20,
     ]);
     // SHA-256 produces hash output of 32 bytes
     let hash_len = Some(32);
-    let padding = Pkcs1v15Sign {hash_len: hash_len, prefix: prefix};
+    let padding = Pkcs1v15Sign {
+        hash_len: hash_len,
+        prefix: prefix,
+    };
     let result = public_key.verify(padding, &hash, &signature);
     result.is_ok()
 }
@@ -101,10 +111,9 @@ fn verify_from_address(dkim: &DKIM) -> bool {
         println!("Email address is not from x.com.");
         return false;
     }
-    
+
     true
 }
-
 
 fn verify_pw_reset_email(dkim: &DKIM) -> bool {
     // Verify the subject in the headers
@@ -120,14 +129,14 @@ fn verify_pw_reset_email(dkim: &DKIM) -> bool {
 
 fn get_twitter_username(dkim: &DKIM) -> String {
     let re = Regex::new(r"This email was meant for (@\w+)").unwrap();
-    
+
     // If "This email was meant for @username" found.
     if let Some(captures) = re.captures(&dkim.body) {
         if let Some(username) = captures.get(1) {
             return username.as_str().to_string();
         }
     }
-    
+
     // If no username found, return empty string.
     String::new()
 }
